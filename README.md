78c2b3812a975a20b18dd3d09372fdf640f4348c
baier2u <geoffrey.baier7@etu.univ-lorraine.fr>
2017-11-04 17:00:01 +0100

    Updating download and readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Framework
 
-Baier Geoffrey
-Binet Paul
+By Baier Geoffrey and Binet Paul
+
+## Introduction
+
+Our framework has been developed to make your website attractive and easy to use.
+It has several appearance and layout rules of different HTML elements such as buttons or alerts.
+For more information see https://baier2u.github.io/Baier_Binet_Framework/
+
+## Download
+Get to https://baier2u.github.io/Baier_Binet_Framework/download.php
+and choose download type file to get Framework files.
---
+++ download.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Framework</title>
+    <meta name="description" content="Home - Framework Baier Binet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
+<h2> Baier Binet Framework </h2>
+<ul class="navbar">
+    <li><a href="index.html"> Home </a> </li>
+    <li><a href="typo.html"> Typography </a> </li>
+    <li><a href="buttons.html"> Buttons </a> </li>
+    <li><a href="alerts.html"> Alerts </a> </li>
+    <li><a href="grid.html"> Grid </a> </li>
+    <li><a href="list.html"> Lists elements </a> </li>
+    <li><a class="active" href="download.php"> Download </a> </li>
+</ul>
+
+<div class="container">
+    <h5>Please choose the download type file.</h5>
+    <?php
+        echo '<a href="https://github.com/baier2u/Baier_Binet_Framework/archive/v1.0.0-beta.zip"><button>Download (.zip)</button></a>';
+        echo '<a href="https://github.com/baier2u/Baier_Binet_Framework/archive/v1.0.0-beta.tar.gz"><button>Download (.tar.gz)</button></a>';
+    ?>
+    <h5>Then, include files in your project and use Framework CSS or SASS version to develop your Website.</h5>
+</div>
+</body>
+</html>
